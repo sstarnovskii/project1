3917e5ae0195eafad147dfc22aa2b50b2e37afea
sstarnovskii <kopernik.t.800@gmail.com>
2019-09-27 22:14:00 +0700

    removed "sex" input

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 age = int(input("Ваш возраст, полных лет: "))
 height = input("Ваш рост, в см: ")
 weight = input("Ваш вес, в кг: ")
-sex = input("Ваш пол (М или Ж): ")
 lifestyle = int(input("Ваш образ жизни по шкале от 1 до 10, где 1 - полностью сидячий, неактивный образ жизни,\n10 - очень активный, спортивный образ жизни: "))
 height = float(height)/100
 imt = float(weight)/(float(height)**2)
+
 if lifestyle <= 4:
     addwater = 0
-elif 4 <= lifestyle <=8:
+elif 4 <= lifestyle <= 8:
     addwater = 2
 else: addwater = 4
 
