8df28c33750c5d583c77799db353ea61ebf2a561
sstarnovskii <kopernik.t.800@gmail.com>
2019-09-27 20:16:01 +0700

    upd 2

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ age = input("Ваш возраст, полных лет: ")
 height = input("Ваш рост, в см: ")
 weight = input("Ваш вес, в кг: ")
 sex = input("Ваш пол (М или Ж): ")
-lifestyle = input("Ваш образ жизни по шкале от 1 до 10, где 1 - полностью сидячий, неактивный образ жизни,\n10 - очень активный, спортивный образ жизни: ")
+lifestyle = int(input("Ваш образ жизни по шкале от 1 до 10, где 1 - полностью сидячий, неактивный образ жизни,\n10 - очень активный, спортивный образ жизни: "))
 height = float(height)/100
 imt = float(weight)/(float(height)**2)
+if lifestyle <= 4:
+    addwater = 0
+elif  4 <= lifestyle <=8:
+    addwater = 2
+else: addwater = 4
 print("Ваш индекс массы тела - ", round(imt, 2), ".", sep='')
-print("Вам рекомендуется пить", int(round(float(weight)/9)*0.25), "л чистой воды в день.")
+print("Вам рекомендуется пить не менее", int(round(float(weight)/9)*0.25) + addwater, "л чистой воды в день.")
 print(age, height, weight, sex, lifestyle, round(imt, 2))
 
